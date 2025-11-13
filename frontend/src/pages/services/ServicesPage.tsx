01a01b11f94b757ad78c7cd7546a45d87282cfa4
Hop Syder <daoudaabassichristian@gmail.com>
2025-11-13 23:18:22 +0100

    modified:   frontend/src/pages/services/ServicesPage.tsx

--- frontend/src/pages/services/ServicesPage.tsx
+++ frontend/src/pages/services/ServicesPage.tsx
@@ -1,39 +1,39 @@
-import { ServicesHero } from './ServicesHero';
-import { ServicesGrid } from './ServicesGrid';
-import { ProcessSection } from './ProcessSection';
-import { PricingSection } from './PricingSection';
-import { FAQSection } from './FAQSection';
-import { ServicesCTA } from './ServicesCTA';
+import { ServiceHero } from './ServiceHero';
+import { ServiceCards } from './ServiceCards';
+import { ServiceWhyUs } from './ServiceWhyUs';
+import { ServicePricing } from './ServicePricing';
+import { ServiceTestimonials } from './ServiceTestimonials';
+import { ServiceCTA } from './ServiceCTA';
 import { SEO } from '../../components/SEO';
 
 export function ServicesPage() {
   return (
     <>
-      <SEO 
+      <SEO
         title="Nos Services - Nexus Connect | Développez Votre Business en Afrique"
         description="Connexion réseau, accompagnement business et formation pour entrepreneurs africains. Développez votre activité avec Nexus Connect : mentorat, networking, ressources exclusives."
         keywords="services entrepreneurs afrique, accompagnement business, formation professionnelle, networking afrique, mentorat startup, développement business afrique"
         url="/#services"
       />
-      
+
       <div className="min-h-screen">
         {/* Hero Section */}
-        <ServicesHero />
+        <ServiceHero />
 
         {/* Services Grid - 4 services principaux */}
-        <ServicesGrid />
+        <ServiceCards />
 
         {/* Process Section - Étapes du processus */}
-        <ProcessSection />
+        <ServiceWhyUs />
 
         {/* Pricing Section - 3 packs tarifaires */}
-        <PricingSection />
+        <ServicePricing />
 
         {/* FAQ Section - Questions fréquentes */}
-        <FAQSection />
+        <ServiceTestimonials />
 
         {/* CTA Final - Appel à l'action puissant */}
-        <ServicesCTA />
+        <ServiceCTA />
       </div>
     </>
   );
